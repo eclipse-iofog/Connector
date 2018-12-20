e943d31b2c56bad84b7bcfe1fcedac9c1913d4cc
Ian <ianmathieu@gmail.com>
2018-12-20 03:57:20 -0800

    Github Slack Integration, paired with some cleaner bash scripts for the connector CI system to be propogated to the other two main repos

---
+++ scripts/pushyank.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+RETURN_STRING="cd /iofog-connector-packaging; fpm -s dir -t deb -n \"iofog-connector\"
+      -v $VERSION -a all --deb-no-default-config-files --after-install debian.sh --after-remove
+      remove.sh --before-upgrade upgrade.sh --after-upgrade debian.sh etc usr; "
+
+declare -a UBUNTU_VERS=("precise" "trusty" "utopic" "vivid" "wily" "xenial") #Support ubuntu versions
+declare -a DEBIAN_VERS=("wheezy" "jessie" "stretch" "buster") #Also appplies to Raspbian, See related for loop
+declare -a FEDORA_VERS=("22" "23" "24") #Supported Fedora Versions
+declare -a REDHAT_VERS=("6" "7") #Supported Redhat versions
+
+for version in ${UBUNTU_VERS}
+do
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/ubuntu/${version} iofog-connector_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/ubuntu/${version} iofog-connector_${VERSION}_all.deb;"
+done
+
+for version in "${DEBIAN_VERS[@]}"
+do
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/debian/${version} iofog-connector_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/debian/${version} iofog-connector_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/raspbian/${version} iofog-connector_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/raspbian/${version} iofog-connector_${VERSION}_all.deb;"
+done
+
+RETURN_STRING="${RETURN_STRING} cd /iofog-connector-packaging-rpm; fpm -s dir -t rpm -n \"iofog-connector\"
+                -v $VERSION -a all --rpm-os 'linux' --after-install rpm.sh --after-remove remove.sh
+                --before-upgrade upgrade.sh --after-upgrade rpm.sh etc usr;"
+
+for version in ${FEDORA_VERS}
+do
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/fedora/${version} iofog-connector-${VERSION}-1.noarch.rpm;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/fedora/${version} iofog-connector-${VERSION}-1.noarch.rpm;"
+done
+
+for version in ${REDHAT_VERS}
+do
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/el/${version} iofog-connector-${VERSION}-1.noarch.rpm;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/el/${version} iofog-connector-${VERSION}-1.noarch.rpm;"
+done
+
+echo ${RETURN_STRING}
