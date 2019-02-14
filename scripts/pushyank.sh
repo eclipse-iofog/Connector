4751edc8029d2d61768c1ba53d5072b41cc9c3f7
dbusel <10116634+dbusel@users.noreply.github.com>
2019-02-14 13:08:20 +0300

    [skip ci] update pushyank script

--- scripts/pushyank.sh
+++ scripts/pushyank.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RETURN_STRING="cd /iofog-connector-packaging; fpm -s dir -t deb -n \"iofog-connector${DEV}\" -v $VERSION
+RETURN_STRING="cd /iofog-connector-packaging; fpm -s dir -t deb -n \"iofog-connector${ID}\" -v $VERSION
       -a all --deb-no-default-config-files --after-install debian.sh --after-remove
       remove.sh --before-upgrade upgrade.sh --after-upgrade debian.sh etc usr;"
 
@@ -12,33 +12,33 @@ declare -a REDHAT_VERS=("6" "7") #Supported Redhat versions
 
 for version in ${UBUNTU_VERS[@]}
 do
-    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/ubuntu/${version} iofog-connector${DEV}_${VERSION}_all.deb;"
-    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/ubuntu/${version} iofog-connector${DEV}_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/ubuntu/${version} iofog-connector${ID}_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/ubuntu/${version} iofog-connector${ID}_${VERSION}_all.deb;"
 done
 
 for version in "${DEBIAN_VERS[@]}"
 do
-    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/debian/${version} iofog-connector${DEV}_${VERSION}_all.deb;"
-    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/debian/${version} iofog-connector${DEV}_${VERSION}_all.deb;"
-    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/raspbian/${version} iofog-connector${DEV}_${VERSION}_all.deb;"
-    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/raspbian/${version} iofog-connector${DEV}_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/debian/${version} iofog-connector${ID}_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/debian/${version} iofog-connector${ID}_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/raspbian/${version} iofog-connector${ID}_${VERSION}_all.deb;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/raspbian/${version} iofog-connector${ID}_${VERSION}_all.deb;"
 done
 
 RETURN_STRING="$RETURN_STRING cd /iofog-connector-packaging-rpm;
-fpm -s dir -t rpm -n \"iofog-connector${DEV}\" -v $VERSION -a all --rpm-os 'linux' --after-install
+fpm -s dir -t rpm -n \"iofog-connector${ID}\" -v $VERSION -a all --rpm-os 'linux' --after-install
 rpm.sh --after-remove remove.sh --before-upgrade upgrade.sh --after-upgrade
 rpm.sh etc usr;"
 
 for version in ${FEDORA_VERS[@]}
 do
-    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/fedora/${version} iofog-connector${DEV}-${VERSION}-1.noarch.rpm;"
-    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/fedora/${version} iofog-connector${DEV}-${VERSION}-1.noarch.rpm;"
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/fedora/${version} iofog-connector${ID}-${VERSION}-1.noarch.rpm;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/fedora/${version} iofog-connector${ID}-${VERSION}-1.noarch.rpm;"
 done
 
 for version in ${REDHAT_VERS[@]}
 do
-    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/el/${version} iofog-connector${DEV}-${VERSION}-1.noarch.rpm;"
-    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/el/${version} iofog-connector${DEV}-${VERSION}-1.noarch.rpm;"
+    RETURN_STRING="${RETURN_STRING} package_cloud yank iofog/iofog-connector/el/${version} iofog-connector${ID}-${VERSION}-1.noarch.rpm;"
+    RETURN_STRING="${RETURN_STRING} package_cloud push iofog/iofog-connector/el/${version} iofog-connector${ID}-${VERSION}-1.noarch.rpm;"
 done
 
 echo $RETURN_STRING
