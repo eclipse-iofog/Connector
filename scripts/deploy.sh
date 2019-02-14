298ca621cd89b099351ffa070d239e979c796729
dbusel <10116634+dbusel@users.noreply.github.com>
2019-02-14 13:09:37 +0300

    [skip ci] update deploy to dev env

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,4 @@ PASSWORD=$1
 USER=$2
 IP=$3
 
-sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} "service iofog-connector stop; sudo apt-get update; sudo apt-get install --only-upgrade -y iofog-connector${DEV}"
-if [ "$TRAVIS_BRANCH" == "develop" ]; then 
-    sshpass -p ${PASSWORD} scp -o StrictHostKeyChecking=no daemon/target/iofog-connector-daemon-jar-with-dependencies.jar ${USER}@${IP}:/usr/bin/iofog-connectord.jar; 
-    sshpass -p ${PASSWORD} scp -o StrictHostKeyChecking=no client/target/iofog-connector-client-jar-with-dependencies.jar ${USER}@${IP}:/usr/bin/iofog-connector.jar; 
-fi
-sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} "service iofog-connector start"
+sshpass -p ${PASSWORD} ssh -o StrictHostKeyChecking=no ${USER}@${IP} "service iofog-connector stop; sudo apt-get update; sudo apt-get install --only-upgrade -y iofog-connector${ID}; service iofog-connector start"
